4ca642fa37c0f1a95860b377c2cdf80eb6151bae
jhon3242 <rldotnr2@gmail.com>
2023-01-01 16:06:51 +0900

    feat(All) : 뼈대 구현

---
+++ Calculation.js
@@ -0,0 +1,18 @@
+
+function getFirstPayment(asset, month = 36) {
+
+}
+
+function getLastPayment(asset, month = 36) {
+
+}
+
+function getMidDate(asset, month = 36, start = new Date()) {
+
+}
+
+function getInstallmentValue(asset, month = 36) {
+
+}
+
+export {getFirstPayment, getLastPayment, getMidDate, getInstallmentValue}
