1852b995f0e5a2f7aca1edc5b3f697cf9cad7dd6
jhon3242 <rldotnr2@gmail.com>
2023-01-01 18:41:23 +0900

    feat(All) : 기본 기능 구현 but 중간 날 구하는곳에서 버그 발견

--- Calculation.js
+++ Calculation.js
@@ -1,20 +1,52 @@
 import * as Util from "./Util.js"
 
 function getFirstPayment(asset, month = 36) {
-
+	return (asset / 2);
 }
 
 function getLastPayment(asset, month = 36) {
-
+	return asset * ((month / 2) - 1) / month;
 }
 
-function getMidDate(asset, month = 36, start = new Date()) {
-
+function getMidPayment(asset, month = 36) {
+	return asset / month;
 }
 
-function getInstallmentValue(asset, month = 36) {
+/**
+ * 중간 납부 날짜를 구하는 함수
+ */
+function getMidDate(start = new Date(), month = 36) {
+	let endDate = Util.getAfterMonth(start, month);
+	let midDateDiff = getMidValue(month, start); // 마지막 일로부터 며칠 전에 납부해야하는지
+	return Util.getDateBefore(endDate, midDateDiff);
+}
 
+/**
+ * 적금 수지 - 첫 달 6/12 - 마지막 달 5/12
+ */
+function getMidValue(month, start) {
+	let year = month / 12;
+	return getInstallmentValue(month, start) 
+	- 6 * year * Util.getDateDiff(start, Util.getAfterMonth(start, month)) 
+	- 5 * year * 1;
+}
+
+/**
+ * 적금 시 총 수지
+ */
+function getInstallmentValue(month = 36, start = new Date()) {
+	let count = -1;
+	let result = 0;
+	let tmpDate = Util.getCloneDate(start);
+	let endDate = Util.getAfterMonth(start, month);
+
+	while (++count < month) {
+		result += Util.getDateDiff(tmpDate, endDate);
+		tmpDate = Util.getAfterMonth(tmpDate, 1);
+	}
+	return result;
 }
 
 
-export {getFirstPayment, getLastPayment, getMidDate, getInstallmentValue}
+
+export {getFirstPayment, getMidPayment, getLastPayment, getMidDate, getInstallmentValue}
--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 # 기능 구현 목록
 
-- [ ] 첫 날 지불해야하는 금액을 구하는 기능 - Calculation#getFirstPayment
-- [ ] 마지막 날 지불해야하는 금액을 구하는 기능 - Calculation#getLastPayment
-- [ ] 선납 이연 시 중간에 입금해야하는 날짜를 구하는 기능 - Calculation#getMidDate
-	- [ ] 두 날짜의 날짜 차이를 구하는 기능 - Util#getDateDiff
-	- [ ] 적금 시 수지를 구하는 기능 - Calculation#getInstallmentValue
+- [X] 첫 날 지불해야하는 금액을 구하는 기능 - Calculation#getFirstPayment
+- [X] 마지막 날 지불해야하는 금액을 구하는 기능 - Calculation#getLastPayment
+- [X] 선납 이연 시 중간에 입금해야하는 날짜를 구하는 기능 - Calculation#getMidDate
+	- [X] 두 날짜의 날짜 차이를 구하는 기능 - Util#getDateDiff
+	- [X] 적금 시 수지를 구하는 기능 - Calculation#getInstallmentValue
 - [X] 시작일로 부터 특정 달 이후의 날짜를 구하는 기능 - Util#getAfterMonth
