92a78ba2091cb26e1724c2d745f2a7092f189e94
jhon3242 <rldotnr2@gmail.com>
2023-01-06 20:07:21 +0900

    refactor : 기본 기능 다른 방법으로 구현

--- Calculation.js
+++ Calculation.js
@@ -1,4 +1,5 @@
 import * as Util from "./Util.js"
+import * as Const from "./Constant.js"
 
 function getFirstPayment(asset, month = 36) {
 	return (asset / 2);
@@ -14,20 +15,53 @@ function getMidPayment(asset, month = 36) {
 
 
 function getLastDate(start = new Date(), month = 36){
-	let tmpDate = Util.getAfterMonth(start, month);
-	tmpDate.setDate(tmpDate.getDate()-  1);
+	let tmpDate = Util.getAfterMonth(start, start.getDate());
+	while (--month > 0) {
+		tmpDate = Util.getAfterMonth(tmpDate, start.getDate());
+	}
 	return tmpDate;
 }
 
 /**
  * 중간 납부 날짜를 구하는 함수
  */
-function getMidDate(6) {
-	let endDate = Util.getAfterMonth(start, month);
-	let midDateDiff = getMidValue(month, start); // 마지막 일로부터 며칠 전에 납부해야하는지
-	return Util.getBeforeDate(endDate, midDateDiff);
+function getMidDate(start, end, month) {
+	let midPayIndex = month * Const.FIRST_RATIO;
+	let info = getPayInfo(start, end, month);
+	let totalDiff = getTotalDiff(info);
+	let midDateInfo = info[midPayIndex];
+	midDateInfo["diff"] = -totalDiff;
+	midDateInfo["deferredPayDate"] = Util.getDateStr(Util.getDateAfter(new Date(midDateInfo["normalPayDate"]), -totalDiff));
+	return midDateInfo["deferredPayDate"];
 }
 
+
+function getPayInfo(start, end, month) {
+	let tmpDate = Util.getCloneDate(start);
+	let lastPayDate = Util.getDateAfter(end, -1);
+	let info = [];
+	let i = 0;
+	while (i++ < month) {
+		let tmp = {"normalPayDate" : null, "deferredPayDate" : null, "diff" : null};
+		tmp["normalPayDate"] = Util.getDateStr(tmpDate);
+		if (i <= month * Const.FIRST_RATIO) {
+			tmp["deferredPayDate"] = Util.getDateStr(start);
+			tmp["diff"] = Util.getDateDiff(new Date(tmp["normalPayDate"]), new Date(tmp["deferredPayDate"]));
+		} else if (i === month * Const.FIRST_RATIO + 1) {
+			tmp["deferredPayDate"] = undefined;
+		} else {
+			tmp["deferredPayDate"] = Util.getDateStr(lastPayDate);
+			tmp["diff"] = Util.getDateDiff(new Date(tmp["normalPayDate"]), new Date(tmp["deferredPayDate"]));
+		}
+		info.push(tmp);
+		tmpDate = Util.getAfterMonth(tmpDate, start.getDate());
+	}
+	return info;
+}
+
+function getTotalDiff(info) {
+	return info.reduce((acc, now) => now["diff"] + acc, 0);
+}
 
 /**
  * 적금 시 총 수지 (만기누적지수)
@@ -51,4 +85,4 @@ function getMidDate(6) {
 // }
 
 
-export {getFirstPayment, getMidPayment, getLastPayment, getMidDate, getLastDate, getInstallmentValue}
+export {getFirstPayment, getMidPayment, getLastPayment, getMidDate, getLastDate}
---
+++ Constant.js
@@ -0,0 +1,6 @@
+
+const FIRST_RATIO = 6 / 12;
+const MID_RATIO = 1 / 12;
+const LAST_RATIO = 5 / 12;
+
+export {FIRST_RATIO, MID_RATIO, LAST_RATIO}
