c63143a0ebbc34b17ddc25f97bcb45765acadbdd
jhon3242 <rldotnr2@gmail.com>
2023-01-17 17:31:03 +0900

    feat(Index) : 로그인 페이지 구현

--- UserController.js
+++ UserController.js
@@ -1,26 +1,10 @@
-import * as InputView from "./view/InputView";
-import {User} from "./domain/User"
+import * as InputView from "./view/InputView.js";
+import {User} from "./domain/User.js"
+import {initAccount} from "./repository/AccountRepository.js";
 
-function init() {
-	let reposiotry = new Map();
-	let id = 0;
 
-	function makeUser(asset, startDate, month) {
-		if (makeUser.length == 3) {
-			let obj = {asset, startDate, month};
-			reposiotry.set(id++, obj);
-			return obj;
-		} else {
-			throw new Error("Invalid format");
-		}
-		
-	}
-
-	return makeUser;
-}
-
-function makeAccount() {
-	let user = new User(InputView.inputName(),
+function getNewUser() {
+	return new User(InputView.inputName(),
 		InputView.inputPassword(),
 		InputView.inputEmail()
 	)
