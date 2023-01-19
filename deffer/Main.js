96a654705ec6afe50694ae352c3d410a6ee2d795
jhon3242 <rldotnr2@gmail.com>
2023-01-19 15:55:54 +0900

    feat(Deffer) : 페이지 이동 구현

--- deffer/Main.js
+++ deffer/Main.js
@@ -3,13 +3,13 @@
 
 
 import * as Cal from "./Calculation.js"
-import {init} from "../domain/User.js";
+import { DefferUser } from "../domain/DefferUser.js";
+// import {User} from "../domain/User.js";
 
 const ASSET = 44992000 + 40256000; // 85248000
 const MONTH = 36;
 const START_DATE = "2023-01-01";
 const print = console.log;
-const makeUser = init();
 
 // const ASSET = document.getElementById("price"); // 85248000
 // const MONTH = document.getElementById("month");
@@ -21,9 +21,12 @@ const makeUser = init();
 
 run();
 
+function makeUser() {
+	return new DefferUser(ASSET, START_DATE, MONTH);
+}
+
 function run() {
 	let user = makeUser(ASSET, START_DATE, MONTH);
-	let user2 = makeUser(ASSET, MONTH);
 	Cal.initUser(user);
 	Cal.calculate(user);
 	print(user.toString());
---
+++ domain/DefferUser.js
@@ -0,0 +1,6 @@
+export function DefferUser(asset, startDate, month) {
+	this.asset = asset;
+	this.startDate = startDate;
+	this.month = month;
+}
+
